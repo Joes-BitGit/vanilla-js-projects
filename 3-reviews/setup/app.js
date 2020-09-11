cbe99ded23abebc39620c741e339c84a1b2e1bd4
Joes-BitGit <josephnalmeida@gmail.com>
2020-09-10 18:55:48 -0700

    Finished Review page

--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -37,3 +37,60 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// Select Items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randBtn = document.querySelector('.random-btn');
+
+
+// Set Starting item
+let currentItem = 0;
+
+// load init item
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentItem);
+});
+
+// show person base on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function (e) {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener('click', function (e) {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// show random persons
+randBtn.addEventListener('click', function (e) {
+  showPerson(getRandomNumber());
+});
+
+function getRandomNumber() {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  return currentItem;
+}
