7258a027c564c6f7f84f86fadc85a258d527f47b
Joes-BitGit <josephnalmeida@gmail.com>
2020-09-12 18:40:22 -0700

    Finished FAQs JS w/ 2 versions DOM traversal & Selectors

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,2 +1,36 @@
 //using selectors inside the element
 // traversing the dom
+
+// traverse DOM method
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+// using selectors 
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function () {
+        // iterate over the articles
+        // if the article is not the same 
+        // then remove the show-text class
+        questions.forEach(function (q) {
+
+            if (q !== question) {
+                q.classList.remove('show-text');
+            }
+        })
+
+
+
+        question.classList.toggle('show-text');
+    });
+});
